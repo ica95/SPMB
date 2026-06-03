80cb0ccdee77dd70a22a3efdc99be0e4e626d525
ica95 <ichanr1810@gmail.com>
2026-06-03 15:29:27 +0800

    seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,8 +13,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::firstOrCreate(
+            ['email' => 'admin@spmbmuh2.web.id'],
+            [
+                'name' => 'Administrator',
+                'password' => Hash::make('adminspmb@'),
+                'role' => 'admin',
+            ]
+        );
 
+        // \App\Models\User::factory(10)->create();
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
